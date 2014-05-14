b1059893a60a60e200341f43fe3a478dd7ec23ee
g <geoff@vision.ai>
2014-05-14 13:31:16 -0400

    remove unused POST, port SessionParams

--- Handler/SessionParams.hs
+++ Handler/SessionParams.hs
@@ -1,9 +1,19 @@
 module Handler.SessionParams where
 
 import Import
+import Helper.Shared
 
-getSessionParamsR :: SessionId -> Handler Html
-getSessionParamsR = error "Not yet implemented: getSessionParamsR"
 
-postSessionParamsR :: SessionId -> Handler Html
-postSessionParamsR = error "Not yet implemented: postSessionParamsR"
+
+getSessionParamsR :: SessionId -> Handler String
+getSessionParamsR sid = do
+    addHeader "Access-Control-Allow-Origin" "*"
+    addHeader "Content-Type" "application/json"
+    liftIO $ print sid
+    let req = object ["command" .= update_params, "session_id" .= ("sessions/" ++ sid)]
+    response <- getPipeResponse req sid
+    return response
+    where
+        update_params :: String
+        update_params = "update_params"
+
