ea7c693847fcced7cfd855dd27edb00130c5217f
Geoff Golder <gdoteof@gmail.com>
2014-07-26 07:54:23 -0700

    adding useful shell scripts
    enter.sh enters you into the container on a bash prompt rather than running yesod devel directly.
    
    yesod_devel.sh runs yesod_devel inside the docker container for vmxmiddle

---
+++ yesod_devel.sh
@@ -0,0 +1,1 @@
+docker run -t -i  -p 3000:3000 -v `pwd`:/code --link db:db  -v /dockerscratch/sessions:/www/vmx/sessions --volumes-from vmxmodels --volumes-from vmxserver --volumes-from vmxdata   gdoteof/d-vmxmiddle  /bin/bash -c "cd /code && cabal clean && yesod devel"
