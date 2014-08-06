df8c329f35ca4b561446db16a0e1922103d37910
g <geoff@vision.ai>
2014-08-06 16:05:32 -0400

    ebtter enter script

--- enter.sh
+++ enter.sh
@@ -1,2 +1,2 @@
 docker rm vmxmiddle-manual
-docker run -t -i  -p 3000:3000 --name vmxmiddle-manual -v `pwd`:/code --link db:db  -v /dockerscratch/sessions:/www/vmx/sessions --volumes-from vmxmodels --volumes-from vmxserver --volumes-from vmxdata   gdoteof/d-vmxmiddle  /bin/bash
+docker run -t -i  --name vmxmiddle-manual -v `pwd`:/code --link db:db  -v /dockerscratch/sessions:/www/vmx/sessions --volumes-from vmxmodels --volumes-from vmxserver --volumes-from vmxdata   gdoteof/d-vmxmiddle  /bin/bash
