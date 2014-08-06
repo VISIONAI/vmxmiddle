b06234ec7966015e972725d5b4207829a06124f4
g <geoff@vision.ai>
2014-08-06 15:30:03 -0400

    first attempt at mac build

---
+++ Setup.hs
@@ -0,0 +1,18 @@
+-- Example Setup.hs for the wxHello app.
+
+import Distribution.MacOSX
+import Distribution.Simple
+
+main :: IO ()
+main = defaultMainWithHooks $ simpleUserHooks {
+         postBuild = appBundleBuildHook guiApps -- no-op if not MacOS X
+       }
+
+guiApps :: [MacApp]
+guiApps = [MacApp "vmxmiddle"
+                  (Just "resources/WxHello.icns")
+                  Nothing -- Build a default Info.plist for the icon.
+                  [] -- No other resources.
+                  [] -- No other binaries.
+                  ChaseWithDefaults -- Try changing to ChaseWithDefaults
+          ]
