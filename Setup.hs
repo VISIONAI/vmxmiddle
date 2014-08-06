fe2e5a5d6977668b48e1397ceb727a4f9f2c2cbf
Tomasz Malisiewicz <malist@gmail.com>
2014-08-06 17:50:52 -0400

    update setup so it works on tom's laptop

--- Setup.hs
+++ Setup.hs
@@ -4,15 +4,16 @@ import Distribution.MacOSX
 import Distribution.Simple
 
 main :: IO ()
-main = defaultMainWithHooks $ simpleUserHooks {
-         postBuild = appBundleBuildHook guiApps -- no-op if not MacOS X
-       }
+main = do
+         defaultMainWithHooks $ simpleUserHooks {
+           postBuild = appBundleBuildHook guiApps -- no-op if not MacOS X
+         }
 
 guiApps :: [MacApp]
-guiApps = [MacApp "vmxmiddle"
+guiApps = [MacApp "middle"
                   (Just "resources/WxHello.icns")
                   Nothing -- Build a default Info.plist for the icon.
                   [] -- No other resources.
                   [] -- No other binaries.
-                  ChaseWithDefaults -- Try changing to ChaseWithDefaults
+                  (ChaseWith defaultExclusions) -- Try changing to ChaseWithDefaults
           ]
