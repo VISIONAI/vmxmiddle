6591e8c48f05a8bb947b04936235fb461606c3dd
g <geoff@vision.ai>
2014-08-07 17:13:02 -0400

    better devel

--- yesod_devel.sh
+++ yesod_devel.sh
@@ -5,6 +5,7 @@ RUNNING=$(docker ps | awk '{print $NF}' | grep 'vmxmiddle-current$' | wc -l)
 docker run -t -i  -p 3000:3000 --name vmxmiddle-current -v `pwd`:/code --link db:db  -v /dockerscratch/sessions:/www/vmx/sessions --volumes-from vmxmodels --volumes-from vmxserver --volumes-from vmxdata   gdoteof/d-vmxmiddle  /bin/bash -c "cd /code && yesod devel"
 
 if [ $? -ne 0 ]; then
+  docker stop vmxmiddle-current
   docker rm vmxmiddle-current
   docker run -t -i  -p 3000:3000 --name vmxmiddle-current -v `pwd`:/code --link db:db  -v /dockerscratch/sessions:/www/vmx/sessions --volumes-from vmxmodels --volumes-from vmxserver --volumes-from vmxdata   gdoteof/d-vmxmiddle  /bin/bash -c "cd /code && yesod devel"
   fi
