0f1f498b2f6fda41e0e75145e7e766b2ee7c538f
g <geoff@vision.ai>
2014-08-09 19:47:22 -0400

    hookerizing and fixing sessions

--- Handler/SessionParams.hs
+++ Handler/SessionParams.hs
@@ -17,7 +17,7 @@ getSessionParamsR sid = do
     addHeader "Access-Control-Allow-Origin" "*"
      -- trace "start of get session params" $ liftIO $ print sid
     addHeader "Content-Type" "application/json"
-    let req = object ["command" .= command, "session_id" .= ("sessions/" ++ sid)]
+    let req = object ["command" .= command, "session_id" .= sid]
     response <- getPipeResponse req sid
     return response
     where
