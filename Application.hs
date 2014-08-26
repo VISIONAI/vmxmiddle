ea460f895614b3ae015cc711ba8bfb62564d1d02
Tomasz Malisiewicz <malist@gmail.com>
2014-08-26 15:32:19 -0400

    add two handlers and comment out css combining

--- Application.hs
+++ Application.hs
@@ -44,6 +44,8 @@ import Handler.WebSocket
 import Handler.ModelImage
 import Handler.ModelViewer
 import Handler.SessionViewer
+import Handler.VideoViewer
+import Handler.RandomImage
 
 -- This line actually creates our YesodDispatch instance. It is the second half
 -- of the call to mkYesodData which occurs in Foundation.hs. Please see the
