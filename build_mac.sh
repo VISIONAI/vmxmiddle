bc8c2a26e9469fcc4abee8ec6846748619292e94
Tomasz Malisiewicz <malist@gmail.com>
2014-09-07 02:18:46 -0400

    make final scaffolded directories

--- build_mac.sh
+++ build_mac.sh
@@ -1,3 +1,13 @@
 sudo runghc Setup configure --user
 sudo runghc Setup build
 sudo chown -R tomasz *
+mkdir dist/build/middle.app/Contents/MacOS/static
+mkdir dist/build/middle.app/Contents/MacOS/static/dist/
+mkdir dist/build/middle.app/Contents/MacOS/static/fonts/
+cp static/index.html dist/build/middle.app/Contents/MacOS/static/
+cp static/dist/* dist/build/middle.app/Contents/MacOS/static/dist/
+cp static/fonts/* dist/build/middle.app/Contents/MacOS/static/fonts/
+
+
+mkdir dist/build/middle.app/Contents/MacOS/config/
+cp config/*.yml dist/build/middle.app/Contents/MacOS/config/
