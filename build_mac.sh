38b70ab2b4db51e2f7d414100361353c644ec1f9
Tomasz Malisiewicz <malist@gmail.com>
2014-09-16 16:15:01 -0400

    update the mac build with enter_license code

--- build_mac.sh
+++ build_mac.sh
@@ -4,12 +4,18 @@ sudo chown -R tomasz *
 mkdir dist/build/middle.app/Contents/MacOS/static
 mkdir dist/build/middle.app/Contents/MacOS/static/dist/
 mkdir dist/build/middle.app/Contents/MacOS/static/fonts/
+mkdir dist/build/middle.app/Contents/MacOS/static/enter_license/
 cp static/index.html dist/build/middle.app/Contents/MacOS/static/
 cp static/dist/* dist/build/middle.app/Contents/MacOS/static/dist/
 cp static/fonts/* dist/build/middle.app/Contents/MacOS/static/fonts/
 
+cp -R static/enter_license/dist dist/build/middle.app/Contents/MacOS/static/enter_license/
+
 mkdir dist/build/middle.app/Contents/MacOS/config/
-cp config/*.yml dist/build/middle.app/Contents/MacOS/config/
+cp config/settings.yml dist/build/middle.app/Contents/MacOS/config/
+
+#copy over VMXserver
+cp -R /Users/tomasz/projects/VMXserver/build dist/build/middle.app/Contents/MacOS/
 
 #strip binary
 strip dist/build/middle.app/Contents/MacOS/middle
