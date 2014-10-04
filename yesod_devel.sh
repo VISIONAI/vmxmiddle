68114b667c4d2e46f1d30e6fbfec678794105fb6
g <geoff@vision.ai>
2014-10-04 18:08:03 -0400

    make it easier for geoff to switch between docker/ubuntu

--- yesod_devel.sh
+++ yesod_devel.sh
@@ -9,3 +9,6 @@ if [ $? -ne 0 ]; then
   docker rm vmxmiddle-current
   docker run -t -i  -p 3000:3000 --name vmxmiddle-current -v `pwd`:/code --link db:db  -v /dockerscratch/sessions:/www/vmx/sessions --volumes-from vmxmodels --volumes-from vmxserver --volumes-from vmxdata   gdoteof/d-vmxmiddle  /bin/bash -c "cd /code && yesod devel"
   fi
+
+chown -R g:g yesod-devel
+chown -R g:g dist
