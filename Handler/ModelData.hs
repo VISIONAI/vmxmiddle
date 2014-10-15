e012bd6cc20b5ebeebe631e2b38b5ac1ff985f79
Tomasz Malisiewicz <malist@gmail.com>
2014-10-15 15:00:32 -0400

    new handler

---
+++ Handler/ModelData.hs
@@ -0,0 +1,19 @@
+module Handler.ModelData where
+import System.Directory (doesFileExist)
+import Import
+
+
+getModelDataR :: ModelId -> Handler Html
+getModelDataR muid = do
+  wd <- wwwDir
+  let model_file = (wd ++ "models/" ++ muid ++ "/model.data")
+  let default_file = "static/img/missing.jpg"
+  e <- liftIO $ doesFileExist model_file
+  if e
+    then do
+    --liftIO $ print "sending real image.jpg"
+    sendFile typePlain model_file
+    else do
+    --liftIO $ print "sending missing.jpg"
+    sendFile "image/png" default_file
+
