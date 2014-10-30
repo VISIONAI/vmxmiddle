f5ec3a0f76199ee2e8219463a5f187ac63b4de34
Tomasz Malisiewicz <tom@vision.ai>
2014-10-30 06:36:49 -0400

    a slightly modified dockerized cabal_build tool which works in digitalocean

--- cabal_build.sh
+++ cabal_build.sh
@@ -1,4 +1,7 @@
-docker run -t -i  -v `pwd`:/code --link db:db  -v /dockerscratch/sessions:/vmx/sessions --volumes-from vmxmodels --volumes-from vmxserver --volumes-from vmxdata   --rm gdoteof/d-vmxmiddle  /bin/bash -c "cd /code && cabal clean && cabal configure && cabal build"
 
-chown -R g:g yesod-devel
-chown -R g:g dist
+docker run -t -i  -v `pwd`:/code --rm gdoteof/d-vmxmiddle /bin/bash -c "cd /code && cabal clean && cabal configure && cabal build"
+
+USER=`whoami`
+GROUP=`groups | awk '{print($1)}'`
+
+chown -R ${USER}:${GROUP} dist
