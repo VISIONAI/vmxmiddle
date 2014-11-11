511e93e5f6a93f7f8b9b5e3646b497bcbb477bd2
Tomasz Malisiewicz <tom@vision.ai>
2014-11-10 23:53:55 -0500

    add new model stream functionality

--- Application.hs
+++ Application.hs
@@ -44,6 +44,7 @@ import Handler.RandomImage
 import Handler.CheckLicense
 import Handler.ActivateLicense
 import Handler.LoadModel
+import Handler.StreamImages
 
 -- This line actually creates our YesodDispatch instance. It is the second half
 -- of the call to mkYesodData which occurs in Foundation.hs. Please see the
