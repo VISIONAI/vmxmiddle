42a804d74833b2d125291c1fe7c2a4b4b77c6a7d
Geoff Golder <gdoteof@gmail.com>
2015-01-07 13:25:10 -0500

    adding cos header

--- Handler/CheckLicense.hs
+++ Handler/CheckLicense.hs
@@ -35,6 +35,7 @@ instance FromJSON VMXServerMessage where
 
 getCheckLicenseR :: Handler Value
 getCheckLicenseR = do
+    addHeader "Access-Control-Allow-Origin" "*"
     extra <-getExtra
     let vmxPath     = (fromMaybe "/vmx/build" $ extraVmxPath extra)
         licensePath = vmxPath ++ "/.vmxlicense"
