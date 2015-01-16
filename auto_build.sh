9bc0b4c14e735e7411343ccfc0aab8e748637195
Tomasz Malisiewicz <tom@vision.ai>
2015-01-15 19:19:08 -0500

    update auto build

--- auto_build.sh
+++ auto_build.sh
@@ -4,6 +4,11 @@ mkdir -p scratch/config
 cp dist/build/middle/middle scratch/vmx
 cp config/settings.yml config/favicon.ico config/robots.txt scratch/config
 
+BUILD_NAME=`git describe --tags`
+echo build name is $BUILD_NAME
+echo $BUILD_NAME > scratch/version
+
+
 if [ -z "$VMXMIDDLE_BRANCH" ]
 then
   echo "VMXMIDDLE_BRANCH environment variable must be set"
