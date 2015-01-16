f059dd5506a3cadfb49c83bff1ee446962df6a02
root <root@build.vision.ai>
2015-01-15 19:00:44 -0500

    cabal file updates

--- cabal_build.sh
+++ cabal_build.sh
@@ -1,4 +1,5 @@
-docker run -t -i  -v `pwd`:/code --rm gdoteof/d-vmxmiddle /bin/bash -c "cd /code && cabal clean && cabal configure && cabal build"
+docker pull gdoteof/d-vmxmiddle
+docker run -v `pwd`:/code --rm gdoteof/d-vmxmiddle /bin/bash -c "cd /code && cabal clean && cabal configure && cabal build"
 
 USER=`whoami`
 GROUP=`groups | awk '{print($1)}'`
