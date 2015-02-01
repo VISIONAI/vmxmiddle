e979c83d1d917b71d346eb38dab755b8823a33fa
Tomasz Malisiewicz <tom@vision.ai>
2015-01-31 20:16:59 -0500

    not a directory

--- cabal_build.sh
+++ cabal_build.sh
@@ -1,5 +1,5 @@
 docker pull gdoteof/d-vmxmiddle
-docker run -v `pwd`:/code --rm gdoteof/d-vmxmiddle /bin/bash -c "cd /code && cabal clean && cabal configure && cabal build && strip dist/build/middle/middle/"
+docker run -v `pwd`:/code --rm gdoteof/d-vmxmiddle /bin/bash -c "cd /code && cabal clean && cabal configure && cabal build && strip dist/build/middle/middle"
 
 USER=`whoami`
 GROUP=`groups | awk '{print($1)}'`
