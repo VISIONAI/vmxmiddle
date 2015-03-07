6b7f113d7b8f692b8e9146a798bac366a1d3b563
Tomasz Malisiewicz <tom@vision.ai>
2015-03-07 12:31:00 -0500

    add ability to save file as something more meaningful than model.data

--- Handler/ModelData.hs
+++ Handler/ModelData.hs
@@ -1,18 +1,22 @@
 module Handler.ModelData where
 import System.Directory (doesFileExist)
+import Data.Text (concat)
 import Import
 
 
 getModelDataR :: ModelId -> Handler Html
 getModelDataR muid = do
   wd <- wwwDir
+  let filename = pack $ muid ++ ".model.data"
   let model_file = (wd ++ "models/" ++ muid ++ "/model.data")
   let default_file = "static/img/missing.jpg"
   e <- liftIO $ doesFileExist model_file
   if e
     then do
     --liftIO $ print "sending real image.jpg"
-    sendFile typePlain model_file
+    addHeader "Content-Disposition" $ Data.Text.concat
+      ["attachment; filename=\"", filename, "\""]
+    sendFile typeOctet model_file
     else do
     --liftIO $ print "sending missing.jpg"
     sendFile "image/png" default_file
@@ -21,13 +25,17 @@ getModelDataR muid = do
 getCompiledModelDataR :: ModelId -> Handler Html
 getCompiledModelDataR muid = do
   wd <- wwwDir
+  let filename = pack $ muid ++ ".compiled.data"
   let model_file = (wd ++ "models/" ++ muid ++ "/compiled.data")
   let default_file = "static/img/missing.jpg"
   e <- liftIO $ doesFileExist model_file
   if e
     then do
     --liftIO $ print "sending real image.jpg"
-    sendFile typePlain model_file
+    addHeader "Content-Disposition" $ Data.Text.concat
+      ["attachment; filename=\"", filename, "\""]
+    sendFile typeOctet model_file
+
     else do
     --liftIO $ print "sending missing.jpg"
     sendFile "image/png" default_file
