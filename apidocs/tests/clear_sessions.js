ca2b7319ca1de133d035bb194619b0a996ccb6a6
Tomasz Malisiewicz <tom@vision.ai>
2015-08-24 12:47:48 -0400

    clear the sessions

--- apidocs/tests/clear_sessions.js
+++ apidocs/tests/clear_sessions.js
@@ -8,16 +8,39 @@ console.log('Clearing sessions');
 var assert = require('assert');
 var myrequest = require('./vmxtester').myrequest;
 
-myrequest({method: 'GET', url:url+'/session'},function(error, response, body) {
-  assert.equal(response.statusCode, 200, 'Problem with status code');
-  body = JSON.parse(body);
+myrequest({method: 'DELETE', url:url+'/session/bad_evil_id'},function(error, response, body) {
+  assert.equal(response.statusCode, 404, 'Problem with status code');
 
-  for (var i = 0; i < body.data.length; ++i) {
-    myrequest({method: 'DELETE', url:url+'/session/'+body.data[i].id},function(error, response, body) {
-      assert.equal(response.statusCode, 200, 'Problem with status code');
-      //body = JSON.parse(body);
-      //console.log('body is', body);
+  myrequest({method: 'GET', url:url+'/session'},function(error, response, body) {
+    assert.equal(response.statusCode, 200, 'Problem with status code');
+    var sessions = JSON.parse(body).data;
+    
+
+    
+    myrequest({method: 'DELETE', url:url+'/session/bad_evil_id'},function(error, response, body) {
+      assert.equal(response.statusCode, 404, 'Problem with status code');
+      
+
+      for (var i = 0; i < sessions.length; ++i) {
+
+        var id = sessions[i].id;
+        
+        myrequest({method: 'GET', url:url+'/session/'+id},function(error, response, body) {
+          assert.equal(response.statusCode, 200, 'Problem with status code');
+          body = JSON.parse(body);
+          var newid = body.data.id;
+          myrequest({method: 'DELETE', url:url+'/session/'+newid},function(error, response, body) {
+            assert.equal(response.statusCode, 200, 'Problem with status code');
+            body = JSON.parse(body);
+            var newerid = body.data.id;
+            myrequest({method: 'GET', url:url+'/session/'+newerid},function(error, response, body) {
+              assert.equal(response.statusCode, 404, 'Problem with status code');
+              
+            });
+          });
+          
+        });
+      } // end loop
     });
-  }
+  });  
 });
-
