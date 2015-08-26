53bdf8a0dfcb244d0d01be05ed55fbf64c49d0ad
Tomasz Malisiewicz <tom@vision.ai>
2015-08-26 05:18:42 -0400

    use sessions

--- apidocs/tests/creation.js
+++ apidocs/tests/creation.js
@@ -7,45 +7,45 @@ var myrequest = require('./vmxtester').myrequest;
 
 var url = 'http://localhost:3000';
 
-myrequest({method: 'GET', url:url+'/session'},function(error, response, body) {
+myrequest({method: 'GET', url:url+'/sessions'},function(error, response, body) {
   assert.equal(response.statusCode, 200, 'Problem with status code');
   body = JSON.parse(body);
   assert.equal(body.data.length,0,'Not starting with 0 sessions');
 
-  myrequest({method: 'POST', url:url+'/session'},function(error, response, body) {
+  myrequest({method: 'POST', url:url+'/sessions'},function(error, response, body) {
     assert.equal(response.statusCode, 400, 'Problem with status code');
     
-    myrequest({method: 'GET', url:url+'/session'},function(error, response, body) {
+    myrequest({method: 'GET', url:url+'/sessions'},function(error, response, body) {
       assert.equal(response.statusCode, 200, 'Problem with status code');
       body = JSON.parse(body);
       assert.equal(body.data.length,0,'Not starting with 0 sessions');
 
-      myrequest({method: 'POST', url:url+'/session',json:{}},function(error, response, body) {
+      myrequest({method: 'POST', url:url+'/sessions',json:{}},function(error, response, body) {
         assert.equal(response.statusCode, 200, 'Problem with status code');
 
-      myrequest({method: 'POST', url:url+'/session',json:{random_stuff:"truly_random"}},function(error, response, body) {
+      myrequest({method: 'POST', url:url+'/sessions',json:{random_stuff:"truly_random"}},function(error, response, body) {
         assert.equal(response.statusCode, 200, 'Problem with status code');
         console.log('body is', body);
         var id = body.data.id;
         
-        myrequest({method: 'GET', url:url+'/session/'+id},function(error, response, body) {
+        myrequest({method: 'GET', url:url+'/sessions/'+id},function(error, response, body) {
           assert.equal(response.statusCode, 200, 'Problem with status code');
           
-        myrequest({method: 'GET', url:url+'/session/XXX'+id},function(error, response, body) {
+        myrequest({method: 'GET', url:url+'/sessions/XXX'+id},function(error, response, body) {
           assert.equal(response.statusCode, 404, 'Problem with status code');
           
 
 
-        myrequest({method: 'GET', url:url+'/session/'+id+'/'},function(error, response, body) {
+        myrequest({method: 'GET', url:url+'/sessions/'+id+'/'},function(error, response, body) {
           assert.equal(response.statusCode, 200, 'Problem with status code');
           
 
 
 
-          myrequest({method: 'POST', url:url+'/session',json:{id:234}},function(error, response, body) {
+          myrequest({method: 'POST', url:url+'/sessions',json:{id:234}},function(error, response, body) {
             assert.equal(response.statusCode, 400, 'Problem with status code');
             
-            myrequest({method: 'POST', url:url+'/session',json:{"id":234}},function(error, response, body) {
+            myrequest({method: 'POST', url:url+'/sessions',json:{"id":234}},function(error, response, body) {
               assert.equal(response.statusCode, 400, 'Problem with status code');
               
 
