1b92aa2e6baf1ba42dc13d93f0c95076bc58d470
Tomasz Malisiewicz <tom@vision.ai>
2015-09-02 00:43:26 -0400

    config requires config input json

--- Handler/SessionConfig.hs
+++ Handler/SessionConfig.hs
@@ -4,6 +4,20 @@ module Handler.SessionConfig where
 import Import
 import Helper.Shared
 
+data ConfigCommand2 = ConfigCommand2 {
+  configCommand2config :: ConfigCommand
+}
+
+instance FromJSON ConfigCommand2 where
+    parseJSON (Object o) = do
+        ConfigCommand2 <$> (o .: "config")
+    parseJSON _ = mzero
+
+instance ToJSON ConfigCommand2 where
+  toJSON (ConfigCommand2 c) =
+    object ["config" .= c]
+
+
 data ConfigCommand = ConfigCommand {
     configCommandPretrained :: Maybe String,
     configCommandLogImages :: Maybe Bool,
@@ -42,12 +56,12 @@ optionsSessionConfigR _ = do
 
 postSessionConfigR :: SessionId -> Handler TypedContent
 postSessionConfigR sid = do
-    (cc :: ConfigCommand) <- requireJsonBody
+    (cc :: ConfigCommand2) <- requireJsonBody
      -- trace "start of get session params" $ liftIO $ print sid
     addHeader "Content-Type" "application/json"
 
     let req = object ["command" .= command,
-                      "config" .= cc
+                      "config" .= (configCommand2config cc)
                       ]
     response <- getPortResponse req sid
     return response
