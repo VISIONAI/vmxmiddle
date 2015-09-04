1f0693fb5f45c911492ff300957e81717d7dc46b
Tomasz Malisiewicz <tom@vision.ai>
2015-09-04 19:02:16 -0400

    cleanup save session

--- Handler/SaveSession.hs
+++ Handler/SaveSession.hs
@@ -6,22 +6,16 @@ import Import
 import Helper.Shared
 import Data.Aeson.Types
 
--- import Data.Conduit
--- import Data.Conduit.List (consume)
--- import qualified Data.ByteString as S
-
 data SaveSessionCommand = SaveSessionCommand {
     saveSessionName :: Maybe String,
     saveSessionNewUUID :: Maybe Bool
 }
 
 instance FromJSON SaveSessionCommand where
-    -- parseJSON Null = return $ SaveSessionCommand Nothing Nothing
     parseJSON (Object o) =
         SaveSessionCommand <$> (o .:? "name")
-        <*> (o .:? "new_uuid")
+                           <*> (o .:? "new_uuid")
     parseJSON _ = mzero
-    --parseJSON _ = return $ SaveSessionCommand Nothing Nothing
 
 instance ToJSON SaveSessionCommand where
     toJSON (SaveSessionCommand name new_uuid) =
@@ -31,31 +25,16 @@ instance ToJSON SaveSessionCommand where
 
 optionsSaveSessionR :: SessionId -> Handler ()
 optionsSaveSessionR _ = do
-    addHeader "Allow" "POST"
+    addHeader "Allow" "Post"
     addHeader "Access-Control-Allow-Headers" "Authorization,Content-Type"
-    addHeader "Access-Control-Allow-Methods" "GET"
+    addHeader "Access-Control-Allow-Methods" "POST"
     return ()
 
 postSaveSessionR :: SessionId -> Handler TypedContent
 postSaveSessionR sid = do
-    --liftIO $ print "About to get json body"
-    --body <- rawRequestBody $$ consume
-    --liftIO $ print $ "body is" ++ (show $ S.length $ S.concat body)
-    cmd2::Result SaveSessionCommand  <- parseJsonBody
+    cmd2::Result SaveSessionCommand <- parseJsonBody
     let cmd = case cmd2 of
                  Error _ -> SaveSessionCommand Nothing Nothing
                  Success val -> val
-
-    --response <- getPortResponse (toJSON cmd) sid
-    --return response
-
     getPortResponse (toJSON cmd) sid >>= return
     
-
-    
-    --cmd :: SaveSessionCommand <- requireJsonBody
-    --liftIO $ print $ ("cmd is here"::String) ++ (show $ toJSON cmd)
-    --response <- getPortResponse (toJSON cmd) sid
-    --return response
-
-
